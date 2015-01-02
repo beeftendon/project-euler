95403a61e870e29c1d83ca6e5dbfddfb213ce301
eugene <eug.song@gmail.com>
2015-01-01 18:28:44 -0800

    Started on changes using method of counting powers of prime factors to determine total # of factors. Don't actually need to know the factors themselves, just how many there are.

--- Problem0012/Problem0012.py
+++ Problem0012/Problem0012.py
@@ -3,36 +3,53 @@ __author__ = 'eugene'
 from numpy import sqrt
 
 
+triangle_table = []  # list of triangle numbers that have been evaluated so far
+divisor_table = [] # list of lists of divisors corresponding to triangle_table
+prime_list = [2] # list of prime numbers that have been calculated so far
+last_checked_for_prime = 2 # last number that was checked if was prime, kept here to shorten later checks
+
 def euler():
     natural_number = 1
     triangle_number = 1
 
     divisor_count = 1
-    div_count_table = [[1, 1]]  # Lookup table of values and their corresponding # of divisors
-    prime_array = []  # Array of primes that will be filled as the triangle number increases
     while divisor_count <= 500:
         natural_number += 1
         triangle_number += natural_number
 
-        divisor_count = number_of_divisors(triangle_number)
-        if divisor_count > 100:
-            print [triangle_number, divisor_count]
+        divisors = find_prime_factors(triangle_number)
+        divisors.sort()
+        divisor_table = divisor_table + [divisors]
+        divisor_count = len(divisors)
+        if len(divisors) > 100:
+            print [triangle_number, divisors]
 
 
-def number_of_divisors(num):
-    divisor_count = 1
-    num = float(num)
-    for denom in range(2, int(num) + 1):
+def find_prime_factors(num):
+    ### Takes number for which prime factors are to be found
+    ### Returns list of powers of each prime number that correspond to the global prime_list
 
-        if num % denom == 0:
-            if num/denom == denom:
-                divisor_count = divisor_count * 2 + 1
-                return divisor_count
-            elif denom > sqrt(num):
-                divisor_count *= 2
-                return divisor_count
-            else:
-                divisor_count += 1
+    num = float(num) # Cast as float to make calculating square root more precise
+    prime_powers = []
+    prime_iter = prime_list[0]
+    prime_num = prime_iter.next()
+    divided_num = num # Number will be divided by the prime factor each time it is tallied
+    while prime_num <= sqrt(num):
+        power = 0  # Number of times the prime factor divides into the number
+        while divided_num % prime_num == 0:
+            power += 1
+            divided_num /= prime_num
+
+        if power > 0:
+            power += 1  # Add one more because that's how the prime factorialization rule works
+
+        prime_powers += [power]
+
+        try:
+            prime_num = prime_iter.next()
+        except StopIteration:
+            break
+
 
 
 euler()
