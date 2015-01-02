f2e194ed4e74a7418525f0e4b54196d8b9852d47
eugene <eug.song@gmail.com>
2015-01-01 23:09:11 -0800

    Went back to brute force method, wrote simpler code which works now. Not sure what was wrong with the original implementation.

--- Problem0012/Problem0012.py
+++ Problem0012/Problem0012.py
@@ -1,55 +1,28 @@
-__author__ = 'eugene'
+from math import sqrt
 
-from numpy import sqrt
+def count_factors(num):
+    factors = 0
+    for i in range(1,int(sqrt(num))+1):
+        if num % i == 0:
+            factors += 1
 
+    if sqrt(num) == int(sqrt(num)):
+        factors = 2*factors - 1
+    else:
+        factors = 2*factors
 
-triangle_table = []  # list of triangle numbers that have been evaluated so far
-divisor_table = [] # list of lists of divisors corresponding to triangle_table
-prime_list = [2] # list of prime numbers that have been calculated so far
-last_checked_for_prime = 2 # last number that was checked if was prime, kept here to shorten later checks
+    return factors
 
 def euler():
-    natural_number = 1
-    triangle_number = 1
+    triangle_num = 1
+    natural_num = 2
+    factors = 0
+    while factors < 500:
+        triangle_num += natural_num
+        print(triangle_num)
+        natural_num += 1
+        factors = count_factors(triangle_num)
 
-    divisor_count = 1
-    while divisor_count <= 500:
-        natural_number += 1
-        triangle_number += natural_number
-
-        divisors = find_prime_factors(triangle_number)
-        divisors.sort()
-        divisor_table = divisor_table + [divisors]
-        divisor_count = len(divisors)
-        if len(divisors) > 100:
-            print [triangle_number, divisors]
-
-
-def find_prime_factors(num):
-    ### Takes number for which prime factors are to be found
-    ### Returns list of powers of each prime number that correspond to the global prime_list
-
-    num = float(num) # Cast as float to make calculating square root more precise
-    prime_powers = []
-    prime_iter = prime_list[0]
-    prime_num = prime_iter.next()
-    divided_num = num # Number will be divided by the prime factor each time it is tallied
-    while prime_num <= sqrt(num):
-        power = 0  # Number of times the prime factor divides into the number
-        while divided_num % prime_num == 0:
-            power += 1
-            divided_num /= prime_num
-
-        if power > 0:
-            power += 1  # Add one more because that's how the prime factorialization rule works
-
-        prime_powers += [power]
-
-        try:
-            prime_num = prime_iter.next()
-        except StopIteration:
-            break
-
-
+    print factors
 
 euler()
